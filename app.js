19337c8b40ec505d68892c99d1dab72497e070ae
omar <herricheomar@gmail.com>
2024-03-10 15:27:31 +0100

    hna jbt the percentage of the battery b the worst methode because the best practise is using regx :)

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const { exec } = require("child_process");
 
-exec("ls", (err, outPut) => {
+exec("pmset -g ps", (err, outPut) => {
   if (err) {
     console.log(err);
     return;
   }
+outPut=outPut.split('\t')[1].split(';')[0];
   console.log(outPut);
 });
